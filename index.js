cbcbe5c77bbe398f45af254c6bec630ea6d87afd
Sherzod <sherzodqurbonov2001.27.08@gmail.com>
2022-11-02 20:32:13 +0500

    bismillah

---
+++ index.js
@@ -0,0 +1,21 @@
+const tabs = document.querySelector(".tabs");
+const btns = document.querySelectorAll(".button");
+const articles = document.querySelectorAll(".content");
+
+
+
+tabs.addEventListener("click", (event) => {
+    const id = event.target.dataset.id
+
+    if(id){
+        btns.forEach((btn) => {
+            btn.classList.remove("live");
+        })
+        event.target.classList.add("live");
+        articles.forEach((articles) => {
+            articles.classList.remove("live");
+        });
+        const element = document.getElementById(id)
+        element.classList.add("live")
+    }
+})
